740318f6c40508e64e269072c4c28cb8dbb5f81d
harish <hb.harishbalaji@gmail.com>
2016-09-02 11:28:18 -0400

    changed some more

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("its me  isint it ");
+		System.out.println("it is going to be me");
 		System.out.println(" by Adele");
 		System.out.println("Do I love java ?");
 	}
