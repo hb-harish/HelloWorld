b4e60cbe4df2c524938041ddafe8c613751064d3
harish <hb.harishbalaji@gmail.com>
2016-09-02 11:26:17 -0400

    4 lines now

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,7 +4,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("its me");
-		
+		System.out.println(" by Adele");
+		System.out.println("Do I love java ?");
 	}
 
 }
